9976e73fd2d8a64343b9174e3633eac80e0a779d
beeinger <sulecki.maciek@gmail.com>
2023-03-18 22:40:30 +0000

    added basic hasher; refined the example;

--- examples/next-js/app/page.tsx
+++ examples/next-js/app/page.tsx
@@ -11,21 +11,5 @@ async function getQuotes() {
 
 export default async function Page() {
   const quotes = await getQuotes();
-
-  // TODO: Make our MMR work with Next.js
-  // const hasher = ;
-  // const mmr = new CoreMMR(new MMRInMemoryStore(), hasher);
-
-  // for (const quote of quotes) {
-  //   console.log(quote.quote);
-  //   const authHash = await hasher.hash([quote.author]);
-  //   console.log(authHash);
-  //   const append = await mmr.append(authHash);
-  //   console.log(append)
-  // }
-  // const root = await mmr.bagThePeaks();
-  // console.log(root);
-
-  // Forward fetched data to your Client Component
-  return <HomePage quotes={quotes} root={"root"} />;
+  return <HomePage quotes={quotes} />;
 }
