3ca0c16e5ab6270fb4efbe15c60306b06252d4c8
Marcello Bardus <marcellobardus@Marcellos-MBP.lan>
2023-05-05 15:33:28 +0100

    prettier

--- packages/hashes/src/stark-poseidon-hasher.ts
+++ packages/hashes/src/stark-poseidon-hasher.ts
@@ -2,21 +2,20 @@ import { IHasher } from "@herodotus_dev/mmr-core";
 import { poseidonHash } from "micro-starknet";
 
 export class StarkPoseidonHasher extends IHasher {
-	constructor() {
-		super({ blockSizeBits: 252 });
-	}
+  constructor() {
+    super({ blockSizeBits: 252 });
+  }
 
-	hash(data: string[]): string {
-		if (data.length !== 2) throw new Error("Stark Poseidon Hasher only accepts two elements");
+  hash(data: string[]): string {
+    if (data.length !== 2) throw new Error("Stark Poseidon Hasher only accepts two elements");
 
-		const sizeErrorIndex = data.findIndex((e) => this.isElementSizeValid(e) === false);
-		if (sizeErrorIndex > -1)
-			throw new Error(
+    const sizeErrorIndex = data.findIndex((e) => this.isElementSizeValid(e) === false);
+    if (sizeErrorIndex > -1)
+      throw new Error(
         `Stark Poseidon Hasher only accepts elements of size ${this.options.blockSizeBits} bits. Got ${JSON.stringify(
           IHasher.byteSize(data[sizeErrorIndex])
         )}`
-			);
-
-		return '0x' + poseidonHash(BigInt(data[0]), BigInt(data[1])).toString(16);
-	}
+      );
+    return "0x" + poseidonHash(BigInt(data[0]), BigInt(data[1])).toString(16);
+  }
 }
