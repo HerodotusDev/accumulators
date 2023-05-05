ca9ee39beca9d10ef33dbb9e0afb1f1202f75e9a
codyx <codyx@users.noreply.github.com>
2023-05-05 18:39:29 +0400

    Make keccakHasher work for single hashes (such as raw hex rlp)

--- packages/hashes/src/keccak-hasher.ts
+++ packages/hashes/src/keccak-hasher.ts
@@ -14,9 +14,16 @@ export class KeccakHasher extends IHasher {
   }
 
   hash(data: string[]): string {
+    // If the data is empty, return the hash of an empty array
+    if (data.length === 0) return utils.keccak256([]);
+    // If the data has a single element, return the hash of that element using `ethers.js` keccak256
+    if (data.length === 1) {
+      return utils.keccak256(data[0]);
+    }
+
+    // Process the data in chunks of 32 bytes elements
     // Pad elements to 32 bytes
     data = data.map((e) => this.numberStringToBytes32(e));
-
     return keccak256(
       data.map((_) => "bytes32"),
       data
