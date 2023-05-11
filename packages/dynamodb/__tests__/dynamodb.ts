cb29af89d5f6d9c43e884130523a36d3f59ca864
beeinger <sulecki.maciek@gmail.com>
2023-05-11 19:35:14 +0100

    full dynamodb; but tests dont pass; needs investigating;

--- packages/dynamodb/__tests__/dynamodb.ts
+++ packages/dynamodb/__tests__/dynamodb.ts
@@ -1,18 +1,18 @@
-import CoreMMR, { IStore, PrecomputationMMR } from "@herodotus_dev/mmr-core";
+import CoreMMR, { PrecomputationMMR } from "@herodotus_dev/mmr-core";
 import { StarkPedersenHasher } from "@herodotus_dev/mmr-hashes";
 import DynamoDbStore from "../src";
-import { DynamoDB } from "aws-sdk";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 
 const hasher = new StarkPedersenHasher();
+const client = new DynamoDBClient({ region: "eu-west-2" });
+const store = new DynamoDbStore(client, "mmrs");
 
 describe("precomputation", () => {
   const rootAt6Leaves = "0x04a1ae364258121690285af43cd4ee91adfd6a8647211748657d8e66835a20a1";
   let mmr: CoreMMR;
-  let store: IStore;
 
   beforeAll(async () => {
-    const client = new DynamoDB.DocumentClient({ region: "eu-west-2" });
-    store = new DynamoDbStore(client, "mmrs");
+    await store.deleteAll();
   });
 
   beforeEach(async () => {
@@ -30,7 +30,7 @@ describe("precomputation", () => {
     await expect(mmr.bagThePeaks()).resolves.toEqual(rootAt6Leaves);
     const proof = await mmr.getProof(8);
     await expect(mmr.verifyProof(proof, "5")).resolves.toEqual(true);
-  });
+  }, 10000);
 
   it("should precompute from parent tree", async () => {
     const precomputationMmr = await PrecomputationMMR.initialize(store, hasher, mmr.mmrUuid, "precomputed");
@@ -44,5 +44,5 @@ describe("precomputation", () => {
     await expect(precomputationMmr.verifyProof(proof, "5")).resolves.toEqual(true);
 
     await precomputationMmr.close();
-  });
+  }, 10000);
 });
--- packages/dynamodb/src/index.ts
+++ packages/dynamodb/src/index.ts
@@ -1,62 +1,90 @@
-import { DynamoDB } from "aws-sdk";
+import {
+  BatchGetItemCommand,
+  BatchWriteItemCommand,
+  DeleteItemCommand,
+  DynamoDBClient,
+  GetItemCommand,
+  PutItemCommand,
+  ScanCommand,
+} from "@aws-sdk/client-dynamodb";
+import { marshall, unmarshall } from "@aws-sdk/util-dynamodb";
 import { IStore } from "@herodotus_dev/mmr-core";
 
 export default class DynamoDbStore implements IStore {
-  constructor(private db: DynamoDB.DocumentClient, private tableName: string, private primaryKey: string = "key") {}
+  constructor(private db: DynamoDBClient, private tableName: string, private primaryKey: string = "key") {}
 
   async get(key: string): Promise<string> {
-    const result = await this.db
-      .get({
-        TableName: this.tableName,
-        Key: { [this.primaryKey]: key },
-      })
-      .promise()
-      .catch((err) => {
-        throw new Error(err.message);
-      });
+    try {
+      const result = await this.db.send(
+        new GetItemCommand({
+          TableName: this.tableName,
+          Key: marshall({ [this.primaryKey]: key }),
+        })
+      );
 
-    return result.Item?.value;
+      if (!result.Item) return null;
+      const item = unmarshall(result.Item);
+      return item?.value ?? null;
+    } catch (err) {
+      throw new Error(err.message);
+    }
+  }
+
+  async set(key: string, value: string): Promise<void> {
+    try {
+      await this.db.send(
+        new PutItemCommand({
+          TableName: this.tableName,
+          Item: marshall({ [this.primaryKey]: key, value }),
+        })
+      );
+    } catch (err) {
+      throw new Error(err.message);
+    }
+  }
+
+  async delete(key: string): Promise<void> {
+    try {
+      await this.db.send(
+        new DeleteItemCommand({
+          TableName: this.tableName,
+          Key: marshall({ [this.primaryKey]: key }),
+        })
+      );
+    } catch (err) {
+      throw new Error(err.message);
+    }
   }
 
   async getMany(keys: string[]): Promise<Map<string, string>> {
     const resultMap = new Map<string, string>();
     const promises: Promise<any>[] = [];
 
-    for (let i = 0; i < keys.length; i += 25) {
-      const keysBatch = keys.slice(i, i + 25);
+    for (let i = 0; i < keys.length; i += 100) {
+      const keysBatch = keys.slice(i, i + 100);
 
       const requestItems: any = {};
       requestItems[this.tableName] = {
-        Keys: keysBatch.map((key) => ({ [this.primaryKey]: key })),
+        Keys: keysBatch.map((key) => marshall({ [this.primaryKey]: key })),
       };
 
       promises.push(
-        this.db
-          .batchGet({ RequestItems: requestItems })
-          .promise()
-          .then((response) => {
-            response.Responses?.[this.tableName].forEach((item) => resultMap.set(item[this.primaryKey], item.value));
-          })
+        this.db.send(new BatchGetItemCommand({ RequestItems: requestItems })).then((response) => {
+          response.Responses?.[this.tableName].forEach((item) => {
+            const unmarshalledItem = unmarshall(item);
+            resultMap.set(unmarshalledItem[this.primaryKey], unmarshalledItem.value);
+          });
+        })
       );
     }
 
-    await Promise.all(promises).catch((err) => {
+    try {
+      await Promise.all(promises);
+    } catch (err) {
       throw new Error(err.message);
-    });
+    }
 
     return resultMap;
-  }
-
-  async set(key: string, value: string): Promise<void> {
-    await this.db
-      .put({
-        TableName: this.tableName,
-        Item: { [this.primaryKey]: key, value },
-      })
-      .promise()
-      .catch((err) => {
-        throw new Error(err.message);
-      });
   }
 
   async setMany(entries: Map<string, string>): Promise<void> {
@@ -69,28 +97,18 @@ export default class DynamoDbStore implements IStore {
       const requestItems: any = {};
       requestItems[this.tableName] = entriesBatch.map(([key, value]) => ({
         PutRequest: {
-          Item: { [this.primaryKey]: key, value },
+          Item: marshall({ [this.primaryKey]: key, value }),
         },
       }));
 
-      promises.push(this.db.batchWrite({ RequestItems: requestItems }).promise());
+      promises.push(this.db.send(new BatchWriteItemCommand({ RequestItems: requestItems })));
     }
 
-    await Promise.all(promises).catch((err) => {
+    try {
+      await Promise.all(promises);
+    } catch (err) {
       throw new Error(err.message);
-    });
-  }
-
-  async delete(key: string): Promise<void> {
-    await this.db
-      .delete({
-        TableName: this.tableName,
-        Key: { [this.primaryKey]: key },
-      })
-      .promise()
-      .catch((err) => {
-        throw new Error(err.message);
-      });
+    }
   }
 
   async deleteMany(keys: string[]): Promise<void> {
@@ -102,15 +120,54 @@ export default class DynamoDbStore implements IStore {
       const requestItems: any = {};
       requestItems[this.tableName] = keysBatch.map((key) => ({
         DeleteRequest: {
-          Key: { [this.primaryKey]: key },
+          Key: marshall({ [this.primaryKey]: key }),
         },
       }));
 
-      promises.push(this.db.batchWrite({ RequestItems: requestItems }).promise());
+      promises.push(this.db.send(new BatchWriteItemCommand({ RequestItems: requestItems })));
     }
 
-    await Promise.all(promises).catch((err) => {
+    try {
+      await Promise.all(promises);
+    } catch (err) {
       throw new Error(err.message);
-    });
+    }
+  }
+
+  async deleteAll(): Promise<void> {
+    let items: any[];
+    let lastEvaluatedKey: any;
+    const promises: Promise<any>[] = [];
+
+    do {
+      const scanResult = await this.db.send(
+        new ScanCommand({
+          TableName: this.tableName,
+          ExclusiveStartKey: lastEvaluatedKey,
+        })
+      );
+
+      items = scanResult.Items || [];
+      lastEvaluatedKey = scanResult.LastEvaluatedKey;
+
+      for (let i = 0; i < items.length; i += 25) {
+        const itemsBatch = items.slice(i, i + 25);
+
+        const requestItems: any = {};
+        requestItems[this.tableName] = itemsBatch.map((item) => ({
+          DeleteRequest: {
+            Key: marshall({ [this.primaryKey]: unmarshall(item)[this.primaryKey] }),
+          },
+        }));
+
+        promises.push(this.db.send(new BatchWriteItemCommand({ RequestItems: requestItems })));
+      }
+    } while (lastEvaluatedKey);
+
+    try {
+      await Promise.all(promises);
+    } catch (err) {
+      throw new Error(err.message);
+    }
   }
 }
