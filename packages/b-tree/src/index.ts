75b3d9582b7376fcf5e92e591a28ff9a568554bb
Filip Krawczyk <kotmawszy@gmail.com>
2023-07-07 09:17:53 +0200

    Add logger to btree

--- packages/b-tree/src/index.ts
+++ packages/b-tree/src/index.ts
@@ -31,12 +31,12 @@ export enum BTREE_METADATA_KEYS {
 }
 
 interface ILogger {
-  log(...args: any[]): void;
+  log(data: string): void;
 }
 
 class ConsoleLogger implements ILogger {
-  log(...args: any[]) {
-    console.log(...args);
+  log(data: string) {
+    console.log(data);
   }
 }
 
@@ -49,7 +49,7 @@ export class BTree {
     protected readonly NULL_VALUE: string,
     protected readonly CHAIN_ID: number = 1,
     protected readonly PAGE_SIZE: number = 8,
-    protected readonly logger?: ILogger, // TODO: add logger
+    protected readonly logger?: ILogger,
     public readonly treeId: string = ulid()
   ) {
     this.mmr = new CoreMMR(store, hasher, `${this.treeId}:${BTREE_METADATA_KEYS.MAIN_TREE}`);
@@ -108,18 +108,29 @@ export class BTree {
               `${this.treeId}:${BTREE_METADATA_KEYS.PAGES}:${ulid()}`
             )
           : new IncrementalMerkleTree(this.PAGE_SIZE, this.NULL_VALUE, this.hasher, this.store, pageMerkleTreeId);
+      this.logger?.log(
+        (pageMerkleTreeId === undefined ? `Created new` : `Recreated`) +
+          ` merkle tree with id ${merkleTree.treeId.split(":")[2]}`
+      );
 
       // make updates to merkle tree
       const [account, pageRange] = pageName.split(":");
       let merkleRootHash;
       for (const { offset, value } of updates) {
         merkleRootHash = await merkleTree.updateAuthenticated(offset, value);
+        this.logger?.log(
+          `Updated merkle tree with id ${merkleTree.treeId.split(":")[2]} at offset ${offset} with value ${value}`
+        );
       }
 
       // update mmr
       const regionCommittment = this.computeRegionCommittment(pageRange, account, merkleRootHash);
+      this.logger?.log(
+        `Computed region committment for page ${pageRange} and account ${account} to be ${regionCommittment}`
+      );
       if (pageMerkleTreeId === undefined) {
         const appendResult = await this.mmr.append(regionCommittment);
+        this.logger?.log(`Appended region committment to mmr at leaf index ${appendResult.leafIndex}`);
         rootHash = appendResult.rootHash;
         await this.store.set(`${merkleTree.treeId}:${BTREE_METADATA_KEYS.MMR_LEAF_INDEX}`, `${appendResult.leafIndex}`);
         await this.store.set(
@@ -129,6 +140,7 @@ export class BTree {
       } else {
         const leafIndex = parseInt(await this.store.get(`${pageMerkleTreeId}:${BTREE_METADATA_KEYS.MMR_LEAF_INDEX}`));
         rootHash = await this.mmr.update(leafIndex, regionCommittment);
+        this.logger?.log(`Updated region committment in mmr at leaf index ${leafIndex}`);
       }
       regionResults.push({
         account: account,
