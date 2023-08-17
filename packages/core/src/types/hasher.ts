9b4034dc6a9f489736ffeade6c9705014b9bbea1
Filip Krawczyk <kotmawszy@gmail.com>
2023-08-17 23:28:58 +0200

    hashSingle and getGenesis functions for hashers

--- packages/core/src/types/hasher.ts
+++ packages/core/src/types/hasher.ts
@@ -22,4 +22,17 @@ export abstract class IHasher {
   public isElementSizeValid = (element: HexString): boolean => IHasher.byteSize(element) <= this.options.blockSizeBits;
 
   static byteSize = (str: string) => new Blob([str.startsWith("0x") ? str.slice(2) : str]).size;
+
+  hashSingle(data: string): string {
+    return this.hash([data]);
+  }
+
+  public getGenesis = (): HexString => {
+    const s = "brave new world";
+    let hex = "0x";
+    for (let i = 0; i < s.length; i++) {
+      hex += s.charCodeAt(i).toString(16).padStart(2, "0");
+    }
+    return this.hashSingle(hex);
+  };
 }
