56192bf27f0da4613d638847291cc3321d15475d
Marcello Bardus <marcellobardus8@gmail.com>
2023-08-23 19:23:42 +0100

    cleanify

--- packages/core/src/types/hasher.ts
+++ packages/core/src/types/hasher.ts
@@ -25,16 +25,16 @@ export abstract class IHasher {
 
   static byteSize = (str: string) => new Blob([str.startsWith("0x") ? str.slice(2) : str]).size;
 
-  hashSingle(data: string): string {
+  public hashSingle(data: string): string {
     return this.hash([data]);
   }
 
-  public getGenesis = (): HexString => {
+  public getGenesis(): HexString {
     const s = GENESIS_STRING;
     let hex = "0x";
     for (let i = 0; i < s.length; i++) {
       hex += s.charCodeAt(i).toString(16).padStart(2, "0");
     }
     return this.hashSingle(hex);
-  };
+  }
 }
