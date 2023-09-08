ea5aec3334541e357070016c3139170dd5bf93c1
Filip Krawczyk <kotmawszy@gmail.com>
2023-09-09 00:53:17 +0200

    Add leading zeros to start poseidon hasher

--- packages/hashers/src/stark-poseidon-hasher.ts
+++ packages/hashers/src/stark-poseidon-hasher.ts
@@ -17,11 +17,11 @@ export class StarkPoseidonHasher extends IHasher {
     const bigintData = data.map((e) => BigInt(e));
 
     if (data.length === 1) {
-      return "0x" + poseidonHashSingle(bigintData[0]).toString(16);
+      return "0x" + poseidonHashSingle(bigintData[0]).toString(16).padStart(63, "0");
     } else if (data.length === 2) {
-      return "0x" + poseidonHash(bigintData[0], bigintData[1]).toString(16);
+      return "0x" + poseidonHash(bigintData[0], bigintData[1]).toString(16).padStart(63, "0");
     } else if (data.length > 2) {
-      return "0x" + poseidonHashMany(bigintData).toString(16);
+      return "0x" + poseidonHashMany(bigintData).toString(16).padStart(63, "0");
     } else throw new Error("Stark Poseidon Hasher only accepts arrays of size 1 or greater");
   }
 }
