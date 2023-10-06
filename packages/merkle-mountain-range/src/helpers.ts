f095140418826183ecbfdcf767bcefb08fc39dc8
Filip Krawczyk <kotmawszy@gmail.com>
2023-10-06 14:26:39 +0200

    Modify helper functions

--- packages/merkle-mountain-range/src/helpers.ts
+++ packages/merkle-mountain-range/src/helpers.ts
@@ -1,42 +1,76 @@
-// Find the peaks (if any) of a tree of size `num`.
-export const findPeaks = (elementsCount: number): number[] => {
-  if (elementsCount === 0) return [];
-
-  // Check for siblings without parents
-  if (getHeight(elementsCount + 1) > getHeight(elementsCount)) return [];
-
-  let top = 1;
-  while (top - 1 <= elementsCount) {
-    top <<= 1;
-  }
-  top = (top >> 1) - 1;
-  if (top === 0) {
-    return [1];
-  }
-
-  const peaks = [top];
-  let peak = top;
-  let outer = true;
-  while (outer) {
-    peak = bintreeJumpRightSibling(peak);
-    while (peak > elementsCount) {
-      peak = bintreeMoveDownLeft(peak);
-      if (peak === 0) {
-        outer = false;
-        break;
-      }
+// Convert number of elements in the MMR to number of leaves.
+// If `elementsCount` is invalid, it throws an error.
+export function elementsCountToLeafCount(elementsCount: number): number {
+  let leafCount = 0;
+  let mountainLeafCount = 1 << bitLength(elementsCount);
+  while (mountainLeafCount > 0) {
+    const mountainElementsCount = 2 * mountainLeafCount - 1;
+    if (mountainElementsCount <= elementsCount) {
+      leafCount += mountainLeafCount;
+      elementsCount -= mountainElementsCount;
     }
-    if (outer) peaks.push(peak);
+    mountainLeafCount >>= 1;
+  }
+  if (elementsCount > 0) {
+    throw new Error(`Invalid elements count: ${elementsCount}`);
+  }
+  return leafCount;
+}
+
+// Find the peaks of a tree of size `elementsCount`.
+// If `elementsCount` is invalid, it returns an empty array.
+export function findPeaks(elementsCount: number): number[] {
+  let mountainElementsCount = (1 << bitLength(elementsCount)) - 1;
+  let mountainIndexShift = 0;
+  const peaks: number[] = [];
+  while (mountainElementsCount > 0) {
+    // console.log("trying mountain size", mountainElementsCount);
+    if (mountainElementsCount <= elementsCount) {
+      mountainIndexShift += mountainElementsCount;
+      peaks.push(mountainIndexShift);
+      elementsCount -= mountainElementsCount;
+    }
+    mountainElementsCount >>= 1;
+  }
+  if (elementsCount > 0) {
+    return [];
   }
   return peaks;
-};
+}
 
 // Returns true if a specified index `num` is also the index of a peak inside `peaks`.
-export const isPeak = (elementIndex: number, peaks: number[]): boolean => peaks.indexOf(elementIndex) !== -1;
+export function isPeak(elementIndex: number, peaks: number[]): boolean {
+  return peaks.indexOf(elementIndex) !== -1;
+}
+
+// For a given number of leaves, calculates how many additional nodes will be created by hashing their children after one append (not including the appended node itself).
+export function leafCountToAppendNoMerges(leafCount: number): number {
+  return countTrailingOnes(leafCount);
+}
 
 // Returns the number of bits in num
 export function bitLength(num: number): number {
   return num.toString(2).length;
+}
+
+// Returns the number of 1 bits in binary representation of num
+export function countOnes(num: number): number {
+  let count = 0;
+  while (num) {
+    num &= num - 1;
+    count++;
+  }
+  return count;
+}
+
+// Returns the number of trailing 1 bits in binary representation of num
+export function countTrailingOnes(num: number): number {
+  let count = 0;
+  while (num && num & 1) {
+    num >>= 1;
+    count++;
+  }
+  return count;
 }
 
 // Number with all bits 1 with the same length as num
@@ -92,19 +126,4 @@ export const siblingOffset = (height: number): number => {
 // Get the offset to the next parent from `height`
 export const parentOffset = (height: number): number => {
   return 2 << height;
-};
-
-// Jump to the next right sibling from `elementIndex`
-const bintreeJumpRightSibling = (elementIndex: number) => {
-  const height = getHeight(elementIndex);
-  return elementIndex + (1 << (height + 1)) - 1;
-};
-
-// Jump down left from `elementIndex`
-const bintreeMoveDownLeft = (elementIndex: number) => {
-  const height = getHeight(elementIndex);
-  if (height === 0) {
-    return 0;
-  }
-  return elementIndex - (1 << height);
 };
