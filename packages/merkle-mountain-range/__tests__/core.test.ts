48502c5701311e74e0749057111e34034b7d3f46
beeinger <sulecki.maciek@gmail.com>
2024-01-17 10:34:27 +0100

    Full test of event from tx (Goerli): 0x7faa311f40d173beda8748eb03072bf0cd9047f45e4daf56c0540b3260ec075e

--- packages/merkle-mountain-range/__tests__/core.test.ts
+++ packages/merkle-mountain-range/__tests__/core.test.ts
@@ -1,6 +1,6 @@
 import MemoryStore from "@accumulators/memory";
 import { KeccakHasher, PoseidonHasher, StarkPedersenHasher, StarkPoseidonHasher } from "@accumulators/hashers";
-import CoreMMR, { AppendResult } from "../src";
+import CoreMMR, { AppendResult, DraftMMR } from "../src";
 
 describe("core", () => {
   const leaves = ["1", "2", "3", "4", "5"]; // Elements data for this test suite (do not modify).
@@ -19,7 +19,6 @@ describe("core", () => {
     const keccak_hasher = new KeccakHasher();
     const poseidon_hasher = new StarkPoseidonHasher();
 
-
     pedersen_mmr = new CoreMMR(store, pedersen_hasher);
     keccak_mmr = new CoreMMR(store, keccak_hasher);
     poseidon_mmr = new CoreMMR(store, poseidon_hasher);
@@ -33,7 +32,7 @@ describe("core", () => {
       appendsResultsForPoseidon.push(await poseidon_mmr.append(leaf));
     }
   });
-  
+
   //================================================================================================
   // Tests for CoreMMR.createWithGenesis
   //================================================================================================
@@ -50,7 +49,7 @@ describe("core", () => {
     expect(await mmr.rootHash.get()).toEqual(hasher.hash(["1", hasher.getGenesis()]));
   });
 
-   //================================================================================================
+  //================================================================================================
   // Tests for get root hash from createWithGenesis
   //================================================================================================
 
@@ -210,10 +209,11 @@ describe("core", () => {
   // Tests for get and verify proof
   //================================================================================================
 
-
   it("should generate and verify non-expiring proofs for pedersen hash", async () => {
     const proofs = await Promise.all(
-      appendsResultsForPedersen.map(({ elementIndex, elementsCount }) => pedersen_mmr.getProof(elementIndex, { elementsCount }))
+      appendsResultsForPedersen.map(({ elementIndex, elementsCount }) =>
+        pedersen_mmr.getProof(elementIndex, { elementsCount })
+      )
     );
     const verifications = await Promise.all(
       proofs.map((proof, idx) => pedersen_mmr.verifyProof(proof, leaves[idx], { elementsCount: proof.elementsCount }))
@@ -224,7 +224,9 @@ describe("core", () => {
 
   it("should generate and verify non-expiring proofs for keccak hash", async () => {
     const proofs = await Promise.all(
-      appendsResultsForKeccak.map(({ elementIndex, elementsCount }) => keccak_mmr.getProof(elementIndex, { elementsCount }))
+      appendsResultsForKeccak.map(({ elementIndex, elementsCount }) =>
+        keccak_mmr.getProof(elementIndex, { elementsCount })
+      )
     );
     const verifications = await Promise.all(
       proofs.map((proof, idx) => keccak_mmr.verifyProof(proof, leaves[idx], { elementsCount: proof.elementsCount }))
@@ -235,7 +237,9 @@ describe("core", () => {
 
   it("should generate and verify non-expiring proofs for poseidon hash", async () => {
     const proofs = await Promise.all(
-      appendsResultsForPoseidon.map(({ elementIndex, elementsCount }) => poseidon_mmr.getProof(elementIndex, { elementsCount }))
+      appendsResultsForPoseidon.map(({ elementIndex, elementsCount }) =>
+        poseidon_mmr.getProof(elementIndex, { elementsCount })
+      )
     );
     const verifications = await Promise.all(
       proofs.map((proof, idx) => poseidon_mmr.verifyProof(proof, leaves[idx], { elementsCount: proof.elementsCount }))
@@ -275,5 +279,101 @@ describe("core", () => {
 
     await poseidon_mmr.clear();
     await expect(poseidon_mmr.getPeaks()).resolves.toEqual([]);
+  });
+});
+
+describe("Event from tx (Goerli): 0x7faa311f40d173beda8748eb03072bf0cd9047f45e4daf56c0540b3260ec075e", () => {
+  const keccak_hasher = new KeccakHasher();
+  const poseidon_hasher = new StarkPoseidonHasher();
+  const store = new MemoryStore();
+
+  let keccak_mmr: CoreMMR;
+  let poseidon_mmr: CoreMMR;
+
+  beforeAll(async () => {
+    keccak_mmr = await CoreMMR.createWithGenesis(store, keccak_hasher, "keccak");
+    poseidon_mmr = await CoreMMR.createWithGenesis(store, poseidon_hasher, "poseidon");
+  });
+
+  let blocks: Record<number, { keccak: string; poseidon: string }> = {
+    9734438: {
+      keccak: "0xcd5631a363d4c9bfc86d3504102595c39d7cd90a940fd165e1bdd911aa504d0a",
+      poseidon: "0x07b8996d5b585da92efa32a57223dfb28fa12e6c04d36d7edb03690f03bec56",
+    },
+    9734439: {
+      keccak: "0x62154309a502f33764c4ec3267e2cabf561dc9e428b0607f6f458942bbe0e02d",
+      poseidon: "0x312134454804550b4a38e1d60dc1f0be80ff62dfea8f3c6be0c257efce3b833",
+    },
+    9734440: {
+      keccak: "0x5104aee2cb3cc519cca3580144624c197a0e8b80ef080fe29698221f9963207d",
+      poseidon: "0x6f0b4ef760469262221de032372c2a6b47b304a48b632af80611fc2e2e10b56",
+    },
+    9734441: {
+      keccak: "0x09ab9ad1513282a5c1e1b4c15436aee479e9759712ebe6e5dbb02411537633e1",
+      poseidon: "0x7f6d47c24e8723a6d6cf4ef089df0bd3ec710d5448b696e47b037109a1d04ce",
+    },
+    9734442: {
+      keccak: "0x5cb8bb916e22e6ab4c0fca4bebc13b05dcaaa7eccacd7636b755d944de4e9217",
+      poseidon: "0x38e557fbc306cbcb5964a503014b375db68a0c6786fd9c6ffc5cdd14b6c9dfc",
+    },
+    9734443: {
+      keccak: "0x0b756461f355b8fb1a6dfdfe5d943f7c037c62b99e806a579500a8a73821e250",
+      poseidon: "0x54aa6067e8c4f6bcd7c47cf7900df1d960098177e186f0c15b6a7544491b539",
+    },
+    9734444: {
+      keccak: "0x3965b0ccf016b56564129ab0f96400c3a84a8e6fa5d25327a6a1762901ee00e9",
+      poseidon: "0x2f185aa16419cad043ddb0b75a7ba0c4233d51b7fee31f1ad6680f5c2b53677",
+    },
+    9734445: {
+      keccak: "0xbe9e359d2632091546be983f8b6488012d607d56c05599c9347fdfdbd86c1b3f",
+      poseidon: "0x3ff20a1d65c24d07ebedb2de39c0a27e67808b49d1544e8ef972da1d24da302",
+    },
+    9734446: {
+      keccak: "0xe9112c401620687b34b0fc6108f35242d32ff37914e302c423e9134851573f65",
+      poseidon: "0x437048beb7e0b3f95fb670e34ac4bd2f32acf6a8ad3eb5fc08682f285ad805b",
+    },
+    9734447: {
+      keccak: "0xd6b12b6b12b253be08a02293261f71383d6159b6339d6aeab45d91643df19bd0",
+      poseidon: "0x3cd2cd10c8fedcccab3691f9852b25936ef838e0c826e39ecba3354f23664cd",
+    },
+  };
+
+  let finalRoots = {
+    keccak: "0xc87c3ba0942e428ad5432078aa7bb0b9d423616a3a1c8c7fc27b546a81465aaf",
+    poseidon: "0x06bdd6350f4f5600876f13fb1ee9be09565e37f4ab97971268bc0eb2df5ed6b9",
+  };
+
+  it("should compute the same root hash as from chain for keccak hasher", async () => {
+    let draftStore = new MemoryStore();
+    let mmr = await DraftMMR.initialize(draftStore, keccak_hasher, keccak_mmr, "draft_keccak");
+
+    for (let blockNumber of Object.keys(blocks).reverse()) {
+      console.log(blockNumber);
+      let { keccak } = blocks[blockNumber];
+      await mmr.append(keccak);
+    }
+
+    let bag = await mmr.bagThePeaks();
+    let elementsCount = await mmr.elementsCount.get();
+    let calculatedRootHash = await mmr.calculateRootHash(bag, elementsCount);
+
+    expect(calculatedRootHash).toEqual(finalRoots.keccak);
+  });
+
+  it("should compute the same root hash as from chain for poseidon hasher", async () => {
+    let draftStore = new MemoryStore();
+    let mmr = await DraftMMR.initialize(draftStore, poseidon_hasher, poseidon_mmr, "draft_poseidon");
+
+    for (let blockNumber of Object.keys(blocks).reverse()) {
+      let { poseidon } = blocks[blockNumber];
+      await mmr.append(poseidon);
+    }
+
+    let calculatedRootHash = await mmr.calculateRootHash(await mmr.bagThePeaks(), await mmr.elementsCount.get());
+
+    expect(calculatedRootHash).toEqual(
+      //? Strip padding
+      finalRoots.poseidon.replace(/^0x0*/, "0x")
+    );
   });
 });
